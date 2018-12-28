490ee7e50a0aa7c0766e472414992b85306709a0
Tamilselvan <meet2tamil@gmial.com>
2018-12-28 13:06:14 +0530

    configuration

--- ERPProduct/src/com/jarvis/configuration/AppInitializer.java
+++ ERPProduct/src/com/jarvis/configuration/AppInitializer.java
@@ -13,8 +13,9 @@ import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class AppInitializer implements WebApplicationInitializer {
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer implements WebApplicationInitializer {
 
 	@Override
 	public void onStartup(ServletContext arg0) throws ServletException {
@@ -27,6 +28,24 @@ public class AppInitializer implements WebApplicationInitializer {
 		servlet.setLoadOnStartup(1);
 		servlet.addMapping("/");
 
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
